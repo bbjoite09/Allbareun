2a83b57a1c66f82b3004f75cb585cd311377e43a
Heeeon <bbjoite09@naver.com>
2022-03-02 21:47:46 +0900

    Feat : 부모 Mission page UI

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import MissionChild from './src/Pages/Mission/MissionChild';
+import MissionParents from './src/Pages/Mission/MissionParents';
 
 const App = () => {
-  return <MissionChild></MissionChild>;
+  return <MissionParents />;
 };
 
 export default App;
