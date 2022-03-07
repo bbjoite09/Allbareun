3966a4f5987af1db8bccc60cb08fd40fcba144b2
Heeeon <bbjoite09@naver.com>
2022-03-07 23:48:41 +0900

    Feat : Mypage 그래프 추가

--- src/Pages/MyPage.tsx
+++ src/Pages/MyPage.tsx
@@ -8,11 +8,66 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
 import Typography from '../Components/Typography';
 
 const { width } = Dimensions.get('screen');
 
 const MyPage = () => {
+  const data = {
+    labels: ['01/01', '01/21', '02/14', '02/31', '03/15', '03/30'],
+    datasets: [
+      {
+        data: [35, 34.3, 34.5, 34.5, 33.7, 33.8],
+        color: (opacity = 1) => `#9CB96A`,
+        strokeWidth: 2,
+      },
+    ],
+  };
+  const data1 = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
   return (
     <SafeAreaView style={styles.container}>
       <Image
@@ -46,8 +101,25 @@ const MyPage = () => {
           />
         </Pressable>
       </View>
+      <LineChart
+        data={data}
+        width={width - 45}
+        height={220}
+        chartConfig={chartConfig}
+        withVerticalLines={false}
+        style={{ marginTop: 10, marginLeft: -10 }}
+        withShadow={false}
+      />
     </SafeAreaView>
   );
+};
+
+const chartConfig = {
+  backgroundGradientFrom: 'white',
+  backgroundGradientTo: 'white',
+  color: (opacity = 1) => `black`,
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false, // optional
 };
 
 const styles = StyleSheet.create({
